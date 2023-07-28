9675513974d546af02d62ead4e68548d319b6262
Nikita <shpulinn@mail.ru>
2023-07-29 01:02:20 +0200

    DeathState script created

---
+++ Assets/Scripts/States/DeathState.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathState : BaseState
+{
+    public override void Construct()
+    {
+        stateName = "Death";
+    }
+
+    public override void Transition()
+    {
+        ShowDeathScreen();
+    }
+
+    private void ShowDeathScreen()
+    {
+        //UIController.Instance.ShowDeathScreen();
+    }
+}
